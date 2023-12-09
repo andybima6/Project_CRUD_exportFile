8e9efa8d4e1cdcd2753b556fbdeca18281d1ffa8
LAPTOP-3F2K575I\Andy Nugraha Putra <andybima6@gmail.com>
2023-12-09 08:21:11 +0700

    update Project CRUD

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -7,7 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css"
+        integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Project_CRUD</title>
 </head>
@@ -15,12 +19,12 @@
 <body>
     <h1 class="text-center mb-4">Data Pegawai</h1>
     <div class="container">
-    <a href = "tambahpegawai" type="button" class="btn btn-success mb-2">Tambah Data +</a>
+        <a href = "tambahpegawai" type="button" class="btn btn-success mb-2">Tambah Data +</a>
         <div class="row">
             @if ($message = Session::get('success'))
-            <div class="alert alert-success" role="alert">
-                {{ $message }}
-              </div>
+                <div class="alert alert-success" role="alert">
+                    {{ $message }}
+                </div>
             @endif
             <table class="table">
                 <thead>
@@ -39,20 +43,22 @@
                         $no = 1;
                     @endphp
                     @foreach ($data as $row)
-                    <tr>
-                        <th scope="row">{{ $no++}}</th>
-                        <td>{{ $row->nama }}</td>
-                        <td>
-                            <img src="{{ asset('fotopegawai/'.$row->foto) }}"  alt="" style = "width:40px;">
-                        </td>
-                        <td>{{ $row->jeniskelamin }}</td>
-                        <td>{{ $row->notelpon }}</td>
-                        <td>{{ $row->created_at->format('D M Y')}}</td>
-                        <td>
-                            <a href = "/tampilkandata/{{ $row->id }}" class="btn btn-info">Edit</a>
-                            <a href = "/deletedata/{{ $row->id }}"type="button" class="btn btn-danger">Delete</a>
-                        </td>
-                    </tr>
+                        <tr>
+                            <th scope="row">{{ $no++ }}</th>
+                            <td>{{ $row->nama }}</td>
+                            <td>
+                                <img src="{{ asset('fotopegawai/' . $row->foto) }}" alt=""
+                                    style = "width:40px;">
+                            </td>
+                            <td>{{ $row->jeniskelamin }}</td>
+                            <td>{{ $row->notelpon }}</td>
+                            <td>{{ $row->created_at->format('D M Y') }}</td>
+                            <td>
+                                <a href = "/tampilkandata/{{ $row->id }}" class="btn btn-info">Edit</a>
+                                <a href = "#"type="button" class="btn btn-danger delete"
+                                    data-id="{{ $row->id }}" data-nama="{{ $row->nama }}">Delete</a>
+                            </td>
+                        </tr>
                     @endforeach
 
 
@@ -63,9 +69,49 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+        integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"
+        integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </body>
+<script>
+    $('.delete').click(function() {
+        var pegawaiid = $(this).attr('data-id')
+
+        var nama = $(this).attr('data-nama')
+        swal({
+                title: "Are you sure?",
+                text: "Once deleted, you will not be able to recover these imaginary files. Are you sure to delete the data with id " +
+                    nama + "",
+                icon: "warning",
+                buttons     : true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    window.location = "/deletedata/" + pegawaiid + ""
+                    swal("Poof! Your imaginary file has been deleted!", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Your imaginary file is safe!");
+                }
+            });
+    });
+</script>
+
+<script>
+    @if (Session::has('success'))
+        toastr.success("{{ Session::get('success') }}")
+    @endif
+</script>
 
 </html>
